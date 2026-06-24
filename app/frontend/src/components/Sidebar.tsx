555d6cf40f95c30f05aaac400a7d7b4ec8289a9e
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2026-06-24 15:34:28 +0000

    feat: enhance Sidebar component layout and styling

--- app/frontend/src/components/Sidebar.tsx
+++ app/frontend/src/components/Sidebar.tsx
@@ -1,7 +1,16 @@
+import logo from "../assets/scales-justice-symbol-law-legal-system.png"
+
 export default function Sidebar() {
     return (
-        <aside className="history-sidebar">
-            sidebar
+        <aside>
+            <div className="sidebar-header">
+                <img src={logo} alt="logo" className="logo-sidebar"/>
+                <h3>Lawya</h3>
+            </div>
+            <hr />
+            <div>
+                <h2>History</h2>
+            </div>
         </aside>
     )
 }
